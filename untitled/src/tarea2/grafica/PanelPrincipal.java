2cab97965b1a4df5ff22c37d0ea5c573941dae9d
Matatatatatata <matias.m.sotomayor@gmail.com>
2023-05-22 00:04:16 -0400

    fondo ajustado quedo filete

--- untitled/src/tarea2/grafica/PanelPrincipal.java
+++ untitled/src/tarea2/grafica/PanelPrincipal.java
@@ -12,8 +12,8 @@ public class PanelPrincipal extends JPanel {
         // Fondo
         System.out.println("PATH: " + getClass().getResource("/Resources/fondo.png"));
 
-        // JLabel wallpaper = new JLabel(new ImageIcon(getClass().getResource("/Resources/fondo.png").getImage().getScaledInstance(1200, 720, Image.SCALE_SMOOTH)));
-        JLabel wallpaper = new JLabel(new ImageIcon(getClass().getResource("/Resources/fondo.png")));
+        JLabel wallpaper = new JLabel(new ImageIcon(new ImageIcon(getClass().getResource("/Resources/fondo.png")).getImage().getScaledInstance(1200, 720, Image.SCALE_SMOOTH)));
+        // JLabel wallpaper = new JLabel(new ImageIcon(getClass().getResource("/Resources/fondo.png")));
         wallpaper.setBounds(0, 0, 1200, 720);
 
         add(wallpaper);
